507ae661dac9f99ad408f00c2b987467d6636710
aattoa <aattoa@proton.me>
2024-01-17 16:26:52 +0200

    cppunittest_add_tests: Add WORKING_DIR parameter

--- CMake/cppunittest_add_tests.cmake
+++ CMake/cppunittest_add_tests.cmake
@@ -1,38 +1,42 @@
 # Script parameters:
-# - EXECUTABLE:    The test executable
-# - CTEST_INCLUDE: The file to be included by CTest
+# - EXECUTABLE:    Path to the test executable
+# - CTEST_INCLUDE: Path to the CMake script file to be included by CTest
+# - WORKING_DIR:   Path to the directory from which the test will be run
 
 if (NOT EXISTS "${EXECUTABLE}")
     message(FATAL_ERROR "Executable '${EXECUTABLE}' does not exist")
 endif ()
-
-if (${CTEST_INCLUDE} STREQUAL "")
+if (NOT CTEST_INCLUDE)
     message(FATAL_ERROR "No CTest include path provided")
 endif ()
+if (NOT WORKING_DIR)
+    message(FATAL_ERROR "No working directory provided")
+endif ()
 
-# Get the list of tests from the executable
+# Get a newline-separated list of tests from the executable.
 execute_process(
     COMMAND "${EXECUTABLE}" --list-tests
     OUTPUT_VARIABLE output
     RESULT_VARIABLE result
-    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+    WORKING_DIRECTORY "${WORKING_DIR}")
 
 if (NOT ${result} EQUAL 0)
     message(FATAL_ERROR "Could not retrieve tests from ${EXECUTABLE}")
 endif ()
 
-# Prepare the list for iteration
+# Prepare the list for iteration.
 string(REPLACE "\n" ";" output "${output}")
 
-# Accumulator for the `add_test` calls
+# Accumulator for the CTest include script content.
 set(script)
 
-# Register each of the discovered tests
-foreach (line IN LISTS output)
-    if (NOT "${line}" STREQUAL "")
-        set(script "${script}add_test(\"${line}\" \"${EXECUTABLE}\" --run-test \"${line}\")\n")
+# Register the discovered tests.
+foreach (test_name IN LISTS output)
+    if (NOT "${test_name}" STREQUAL "")
+        set(script "${script}add_test(\"${test_name}\" \"${EXECUTABLE}\" --run-test \"${test_name}\")\n")
+        set(script "${script}set_tests_properties(\"${test_name}\" PROPERTIES WORKING_DIRECTORY \"${WORKING_DIR}\")\n")
     endif ()
 endforeach ()
 
-# Write the script to the CTest include file
+# Write the generated script to the CTest include file.
 file(WRITE "${CTEST_INCLUDE}" "${script}")
